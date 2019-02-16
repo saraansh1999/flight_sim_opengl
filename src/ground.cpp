539b3871aceddbe27eb293d53c85e69328ec613c
saraansh <saraanshtandon1999@gmail.com>
2019-02-17 00:17:36 +0530

    Minor mistake corrected.(100 to 1)

--- src/ground.cpp
+++ src/ground.cpp
@@ -101,7 +101,7 @@ Ground::Ground(float x, float y, float z) {
 }
 
 void Ground::draw(glm::mat4 VP) {
-    Matrices.model = glm::mat4(100.0f);
+    Matrices.model = glm::mat4(1.0f);
     glm::mat4 translate = glm::translate (this->position);    // glTranslatef
     // glm::mat4 rotate    = glm::rotate((float) (this->rotation * M_PI / 180.0f), glm::vec3(1, 0, 0));
     // No need as coords centered at 0, 0, 0 of cube arouund which we waant to rotate
